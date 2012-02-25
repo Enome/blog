2bcad4245897ce85e559febae32b8d25715c93ab
Geert Pasteels <geert.pasteels@gmail.com>
2012-02-25 15:24:23 -0800

    forgot to remove the old file

--- 002_decoupling_with_event_emitters/example_app/database_layer.js
+++
@@ -1,8 +0,0 @@
-exports.init = function(eventEmitter){
-
-  eventEmitter.on('create document', function(document, callback){
-    console.log('Created the following document', document);
-    callback();
-  });
-
-};
